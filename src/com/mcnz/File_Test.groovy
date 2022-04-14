0b6035464f8a1659acd43881cdb3cb8b98e002e5
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-14 16:19:30 +0530

    Second Change Updated

--- src/com/mcnz/File_Test.groovy
+++ src/com/mcnz/File_Test.groovy
@@ -1,8 +1,10 @@
 package com.mcnz
+import groovy.io.FileType
 
 public class FileTest {
 
   def buildrelease() {
+    def fileList = []
     def file = new File("https://github.com/b4u-mark/Shared-Library.git")
     file.eachFileRecurse (FileType.FILES) { rel_file ->
     fileList << rel_file
