0a002dcc4902b4b24ac7adb36329b9d70371f937
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-16 20:24:48 +0530

    Check

--- src/com/mcnz/File_Test.groovy
+++ src/com/mcnz/File_Test.groovy
@@ -4,12 +4,9 @@ import groovy.io.FileType
 public class FileTest {
 
   def buildrelease() {
-    def fileList = []
-    def file = new File("your_repo_dir")
-    file.eachFileRecurse (FileType.FILES) { rel_file ->
-    fileList << rel_file
-    }
-    if (rel_file.exists()){
+    def file = new File("Release_Status.csv")
+    
+    if (file.exists()){
       return true;
     }
     else {
