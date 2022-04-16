6cf6761db240ed33b4f8961bfb73026bbf753a3b
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-16 20:29:35 +0530

    File groovy updated

--- src/com/mcnz/File_Test.groovy
+++ src/com/mcnz/File_Test.groovy
@@ -5,9 +5,11 @@ public class FileTest {
 
   def buildrelease() {
     def file = new File("Release_Status.csv")
+    println "Check file.exists()"
     
     if (file.exists()){
       return true;
+      println "File exists"
     }
     else {
       println "File does not exist."
