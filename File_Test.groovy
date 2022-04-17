25c3fd47729487a53625076cf69a52efc4eeb52f
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-17 17:17:57 +0530

    Checking for another failure

--- File_Test.groovy
+++ File_Test.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
 
-@Grab('com.xlson.groovycsv:groovycsv:0.2')
-import com.xlson.groovycsv.CsvParser
-
 // Jenkinsfile (Declarative Pipeline)
 pipeline {
   agent any
@@ -11,13 +8,16 @@ pipeline {
       steps {
         echo 'Hello world!'
       }
+    }
     stage('Analyze & Report') {
       steps {
         echo 'Hello All!'
-      }
+      } 
+    }
     stage("Email Notification") {
       steps {
         emailext body: '''Hello Your email has configured successfully!!!''', subject: 'Hi This is for email configuration in jenkins', to: 'b4u.mark@gmail.com'
       }
     }
   }
+}
