4df3422a4f115c7c23cd5ff5847d2fb2151739f8
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-17 17:02:04 +0530

    Pipeline Changes

--- src/com/mcnz/File_Test.groovy
+++ src/com/mcnz/File_Test.groovy
@@ -1,21 +1,28 @@
+#!/usr/bin/env groovy
+
+// Jenkinsfile (Declarative Pipeline)
 package com.mcnz
 import groovy.io.FileType
 
 public class FileTest {
 
-  def buildrelease() {
-    def file = new File("Release_Status.csv")
-    println "Check file.exists()"
-    
-    if (file.exists()){
-      return true;
-      println "File exists"
+def buildrelease() {
+pipeline {
+  agent any
+  stages {
+    stage('Checkout Input') {
+      steps {
+        echo 'Hello world!'
+      }
+    stage('Analyze & Report') {
+      steps {
+        echo 'Hello All!'
+      }
+    stage('Email Notification') {
+      steps {
+        emailext body: '''Hello Your email has configured successfully!!!''', subject: 'Hi This is for email configuration in jenkins', to: 'b4u.mark@gmail.com'
+      }
     }
-    else {
-      println "File does not exist."
-    }
-    
-    return false;
   }
-
+}
 }
