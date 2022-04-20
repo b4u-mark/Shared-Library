e9f31761c1d7bb5c6f9705ac2df2b1e5a64b5438
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-20 23:45:06 +0530

    Param check

--- File_Test.groovy
+++ File_Test.groovy
@@ -5,14 +5,14 @@ pipeline {
   stages {
     stage('Checkout Input') {
       steps {
-	  git branch: "${GIT_BRANCH}",
-        url: "${GIT_URL}"
+	  git branch: GIT_BRANCH,
+        url: GIT_URL
       }
     }
     stage('Analyze & Report') {
       steps {
                script {
-			 def fh = readFile(file: "${FILE}")
+			 def fh = readFile(file: FILE)
                    println "The file has ${fh.length()} bytes"
 			 def data = fh.toString()
 			 println data
@@ -21,7 +21,7 @@ pipeline {
     }
     stage('Email Notification') {
       steps {
-        emailext from: "${EmailID}", attachLog: true, body: "Hi, <p> Please find the attached log for details </p>", mimeType: 'text/html', subject: "Reg: Release Log", to: "${EmailID}"
+        emailext from: EmailID, attachLog: true, body: "Hi, <p> Please find the attached log for details </p>", mimeType: 'text/html', subject: "Reg: Release Log", to: EmailID
       }
     }
   }
