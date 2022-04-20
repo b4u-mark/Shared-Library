1f6d13050fb6cbfb68c49aebc61921c257289c8e
b4u-mark <99719790+b4u-mark@users.noreply.github.com>
2022-04-20 23:12:31 +0530

    Parameter

--- File_Test.groovy
+++ File_Test.groovy
@@ -5,14 +5,14 @@ pipeline {
   stages {
     stage('Checkout Input') {
       steps {
-	  git branch: 'main',
-        url: "https://github.com/b4u-mark/Shared-Library.git"
+	  git branch: $GIT_BRANCH,
+        url: $GIT_URL
       }
     }
     stage('Analyze & Report') {
       steps {
                script {
-			 def fh = readFile(file: 'Release_Status.csv')
+			 def fh = readFile(file: $FILE)
                    println "The file has ${fh.length()} bytes"
 			 def data = fh.toString()
 			 println data
@@ -21,7 +21,7 @@ pipeline {
     }
     stage('Email Notification') {
       steps {
-        emailext from: 'jenkinsmarkandrew@gmail.com', attachLog: true, body: "Hi, <p> Please find the attached log for details </p>", mimeType: 'text/html', subject: "Reg: Release Log", to: 'jenkinsmarkandrew@gmail.com'
+        emailext from: $EmailID, attachLog: true, body: "Hi, <p> Please find the attached log for details </p>", mimeType: 'text/html', subject: "Reg: Release Log", to: $EmailID
       }
     }
   }
